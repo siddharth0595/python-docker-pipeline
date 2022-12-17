c0a374b7a8112a824a7e7bb98bcf3cc5f59c1367
siddhath0595 <siddharthchoudhary111@gmail.com>
2022-12-18 00:17:10 +0530

    v1

---
+++ cp.py
@@ -0,0 +1,13 @@
+try:
+	def cp():
+		global b
+		with open(r"/home/ubuntu/f1.txt","w") as i:
+			i.write("hello this is my jenkins pipeline")
+		with open(r"/home/ubuntu/f1.txt","r") as i:
+			b=i.read()
+		with open(r"/home/ubuntu/file2.txt","x") as i:
+			i.write(b)
+	cp()
+except FileExistsError as i:
+	with open(r"/home/ubuntu/file2.txt","a") as i:
+		i.write(b)
